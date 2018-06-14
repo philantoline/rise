9fb4b5d17cd7032c03fb313b82cad8a5e3a094f5
Phil Antoline <phillip.antoline@gmail.com>
2018-06-14 19:16:44 -0400

    rise xocde project and tests

---
+++ rise/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  rise
+//
+//  Created by Phil on 6/13/18.
+//  Copyright © 2018 Phil Antoline. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
